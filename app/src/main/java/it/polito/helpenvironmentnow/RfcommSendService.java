e76ce974f76303ccdc2d23fc12549531c1e668ff
Cosmin Daniel <streetking46cosmin@gmail.com>
2019-10-04 22:12:26 +0200

    Some new comments and some little changes

--- app/src/main/java/it/polito/helpenvironmentnow/RfcommSendService.java
+++ app/src/main/java/it/polito/helpenvironmentnow/RfcommSendService.java
@@ -19,21 +19,20 @@ import android.util.Log;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.InvocationTargetException;
-import java.util.Arrays;
 
 public class RfcommSendService extends IntentService {
 
     private String TAG = "AppHelpNow";
-    private static final int MAX_ATTEMPTS = 10; // the max number to retry establish bluetooth connection if fails
+    private static final int MAX_CONNNECTION_ATTEMPTS = 10; // the max number to retry establish bluetooth connection if fails
     private static final int BLUETOOTH_MSECONDS_SLEEP = 3000; // milliseconds to sleep after connection fails
     private static final int NUMBER_OF_MESSAGES_CHARS = 8; // the number of chars used to represent the total number of messages
     private static final int MESSAGE_SIZE_CHARS = 4; // the number of chars used to represent the size of a message
     private static final int SINGLE_READ_SIZE = 1024; // bytes to read with a single call to "read()" - the same size on server side(SINGLE_WRITE_SIZE)
-    private int numberOfMessages, messageSize;
-    private byte[] messagesRead;
+    private int numberOfMessages, messageSize; // these fields will be set inside "readMetaDataMessages" after receiving them from raspberry
+    private byte[] messagesRead; // contains the sensor data(with timestamps) received from raspberry
 
     public RfcommSendService() {
-        super("Rfcomm Send Service");
+        super("RaspberryToServerService");
     }
 
     @Override
@@ -41,21 +40,22 @@ public class RfcommSendService extends IntentService {
         super.onCreate();
         // A foreground service in order to work in Android has to show a notification, as quoted by
         // the official guide: "Foreground services must display a Notification."
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
-            startMyOwnForeground(); // put the service in a foreground state
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) // check if Android version is 8 or higher
+            startMyOwnForeground(); // put the service in a foreground state - for Android 8+
         else
-            startForeground(1, new Notification()); // put the service in a foreground state
+            startForeground(1, new Notification()); // put the service in a foreground state - until Android 7
     }
 
     @Override
     protected void onHandleIntent(Intent intent) {
-        Log.d(TAG, "onHandleIntent(...) called");
         String remoteDeviceMacAddress = intent.getStringExtra("remoteMacAddress");
         Log.d(TAG,"Mac address from intent: " + remoteDeviceMacAddress);
         boolean result = connectAndReadFromRaspberry(remoteDeviceMacAddress);
-        if(result)
+        if(result) {
             //connectAndSendToServer();
-            Log.d(TAG, "onHandleIntent(...) data:" + Arrays.toString(messagesRead));
+            String sb = new String(messagesRead);
+            Log.d(TAG, "onHandleIntent(...) data:" + sb);
+        }
     }
 
     @RequiresApi(api = Build.VERSION_CODES.O)
@@ -91,16 +91,16 @@ public class RfcommSendService extends IntentService {
         String strMessages = new String(buffer);
         numberOfMessages = Integer.parseInt(strMessages.substring(0,NUMBER_OF_MESSAGES_CHARS));
         messageSize = Integer.parseInt(strMessages.substring(NUMBER_OF_MESSAGES_CHARS, NUMBER_OF_MESSAGES_CHARS + MESSAGE_SIZE_CHARS));
-        Log.d(TAG,"in readMetaDataMessages() numberOfMessages:"+numberOfMessages);
-        Log.d(TAG,"in readMetaDataMessages() messageSize:"+messageSize);
+        Log.d(TAG,"readMetaDataMessages() numberOfMessages:" + numberOfMessages + " messageSize:" + messageSize);
     }
 
+    // This method receives all the messages from raspberry and save them into "messagesRead" array
     private void readMessages(InputStream socketInputStream) throws IOException {
         readMetaDataMessages(socketInputStream);
         int totalDataSize = numberOfMessages * messageSize;
         messagesRead = new byte[totalDataSize];
 
-        Log.d(TAG, "in readMessages() totalDataSize:"+totalDataSize);
+        Log.d(TAG, "readMessages() totalDataSize:"+totalDataSize);
         int currentDataSize = 0, bytesRead;
         int singleReadSize = SINGLE_READ_SIZE;
         while(currentDataSize < totalDataSize) {
@@ -109,45 +109,48 @@ public class RfcommSendService extends IntentService {
             bytesRead = socketInputStream.read(messagesRead, currentDataSize, singleReadSize);
             if(bytesRead != -1)
                 currentDataSize += bytesRead;
-            Log.d(TAG, "in readMessages():" + bytesRead + "currentDataSize:" + currentDataSize);
+            Log.d(TAG, "readMessages():" + bytesRead + " currentDataSize:" + currentDataSize);
         }
     }
 
-    // This method returns TRUE if all the data has ben received and it is ready to be sent to the server
+    // This method returns TRUE if all the data has ben received and are ready to be sent to the server
     private boolean connectAndReadFromRaspberry(String remoteDeviceMacAddress) {
         BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if(bluetoothAdapter == null)
             Log.d(TAG, "bluetoothAdapter is NULL");
         else {
             BluetoothDevice remoteDevice = bluetoothAdapter.getRemoteDevice(remoteDeviceMacAddress);
+            bluetoothAdapter.cancelDiscovery();
             try {
-                bluetoothAdapter.cancelDiscovery();
                 int attempt = 1;
                 boolean connected = false;
-                while(attempt <= MAX_ATTEMPTS && !connected) {
+                while(attempt <= MAX_CONNNECTION_ATTEMPTS && !connected) {
                     BluetoothSocket socket = (BluetoothSocket) BluetoothDevice.class.getMethod(
                             "createInsecureRfcommSocket", int.class).invoke(remoteDevice, 1);
-                    if(socket == null) Log.d(TAG, "socket is NULL");
-                    try {
-                        Log.d(TAG, "Socket connecting attempt:" + attempt);
-                        socket.connect();
-                        connected = true;
-                        InputStream socketInputStream = socket.getInputStream();
-                        readMessages(socketInputStream);
-                        return true;
-                    } catch (IOException e) {
-                        if(attempt < MAX_ATTEMPTS)
-                            SystemClock.sleep(BLUETOOTH_MSECONDS_SLEEP);
-                        Log.d(TAG, "Error with the socket or input stream read!");
-                        e.printStackTrace();
-                        attempt++;
-                    } finally {
+                    if(socket != null) {
                         try {
-                            socket.close();
-                            Log.d(TAG, "Socket closed.");
+                            Log.d(TAG, "Socket connecting attempt:" + attempt);
+                            socket.connect();
+                            connected = true;
+                            InputStream socketInputStream = socket.getInputStream();
+                            readMessages(socketInputStream);
+                            return true;
                         } catch (IOException e) {
+                            if (attempt < MAX_CONNNECTION_ATTEMPTS)
+                                SystemClock.sleep(BLUETOOTH_MSECONDS_SLEEP);
+                            Log.d(TAG, "Error with the socket or input stream read!");
                             e.printStackTrace();
+                            attempt++;
+                        } finally {
+                            try {
+                                socket.close();
+                                Log.d(TAG, "Socket closed.");
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
                         }
+                    } else {
+                        Log.e(TAG, "socket is NULL");
                     }
                 }
             } catch (IllegalAccessException e) {
