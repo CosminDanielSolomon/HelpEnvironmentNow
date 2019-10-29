5f8f17bc6f668cb2a9704dcddab5a0af67c6940e
Cosmin Daniel <streetking46cosmin@gmail.com>
2019-10-30 00:44:02 +0100

    Added backoff criteria

--- app/src/main/java/it/polito/helpenvironmentnow/MyWorker/MyWorkerManager.java
+++ app/src/main/java/it/polito/helpenvironmentnow/MyWorker/MyWorkerManager.java
@@ -2,11 +2,14 @@ package it.polito.helpenvironmentnow.MyWorker;
 
 import android.content.Context;
 
+import androidx.work.BackoffPolicy;
 import androidx.work.Constraints;
 import androidx.work.ExistingWorkPolicy;
 import androidx.work.NetworkType;
 import androidx.work.OneTimeWorkRequest;
 import androidx.work.WorkManager;
+
+import java.util.concurrent.TimeUnit;
 
 public class MyWorkerManager {
     public static void enqueueNetworkWorker(Context context) {
@@ -14,8 +17,9 @@ public class MyWorkerManager {
         // Create a Constraints object that defines when the task should run
         Constraints constraints = new Constraints.Builder().setRequiredNetworkType(NetworkType.CONNECTED).build();
         // A WorkRequest for non-repeating work
-        OneTimeWorkRequest uploadWorkRequest = new OneTimeWorkRequest.Builder(UploadWorker.class)
-                .setConstraints(constraints).build();
+        OneTimeWorkRequest uploadWorkRequest = new OneTimeWorkRequest.Builder(UploadWorker.class).
+                setBackoffCriteria(BackoffPolicy.EXPONENTIAL, 2, TimeUnit.MINUTES).
+                        setConstraints(constraints).build();
         WorkManager.getInstance(context).enqueueUniqueWork(WORK_TAG, ExistingWorkPolicy.REPLACE, uploadWorkRequest);
     }
 }
